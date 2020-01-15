5327084dbc07b2137d3632a8b35fc1624e350a5c
sakuradauverworld <sakuradaasaku@gmail.com>
2020-01-15 17:56:55 +0900

    top2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+   
+  end
+
+
+  def create
+   @posts = Post.create(name: params[:name], image: params[:image])
+    if@posts.save
+      flash[:notice] = "投稿完了"
+      redirect_to ("/")
+    else
+      render("posts/new")
+    end
+  end
+ 
+
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get "/" => "posts#index"
+  get '/' => 'posts#index'
+  get 'posts/new'  => 'posts#new'
+  post 'posts/create' => 'posts#create'
 end
