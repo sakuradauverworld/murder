7c42c7e5386e1d5d6f6eee0cfac7cbbb80849c40
sakuradauverworld <sakuradaasaku@gmail.com>
2020-01-15 20:19:30 +0900

    詳細１

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get '/' => 'posts#index'
   get 'posts/new'  => 'posts#new'
   post 'posts/create' => 'posts#create'
-  get ''
+  get 'posts/:id' => 'posts#show'
 end
